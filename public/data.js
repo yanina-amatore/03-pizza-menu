10470fba9afac2ee7092ba53f9f8d984858f4a4d
Yanina Amatore <yanina.amatore@datacom.co.nz>
2024-02-26 11:33:03 +1300

    setting up pizza project

--- public/data.js
+++ public/data.js
@@ -1,6 +1,3 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-
 const pizzaData = [
   {
     name: "Focaccia",
@@ -45,34 +42,3 @@ const pizzaData = [
     soldOut: false,
   },
 ];
-
-function App() {
-  return <>
-  <h1>Hello React!</h1>
-  <Pizza/>
-  </>
-}
-
-function Pizza (){
-  return <>
-  <img src="pizzas/spinaci.jpg" alt="Pizza spinaci" />
-  <h2>Pizza Spinaci</h2>
-  <p>
-  Tomato, mozarella, spinach, and ricotta cheese
-  </p>
-  </>
-}
-
-// React v.18
-const root = ReactDOM.createRoot(document.getElementById('root'))
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-)
-
-// strictMode during development kit will render the App twice and React will check if we are using outdated parts of the React API.
-
-
-// React before 18
-// React.render(<App />, document.getElementById('root'));
