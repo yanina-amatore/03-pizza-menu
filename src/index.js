d8d98179e6e5af54e08da281509577ca666eb883
Yanina Amatore <yanina.amatore@datacom.co.nz>
2024-02-26 17:21:52 +1300

    props

--- src/index.js
+++ src/index.js
@@ -59,17 +59,7 @@ function App() {
   
 }
 
-function Pizza (){
-  return <>
-  <div className='pizza'>
-    <img src="pizzas/spinaci.jpg" alt="Pizza spinaci" />
-    <h3>Pizza Spinaci</h3>
-    <p>
-    Tomato, mozarella, spinach, and ricotta cheese
-    </p>
-  </div>
-  </>
-}
+
 
 function Header() {
  const style= {} 
@@ -87,12 +77,24 @@ function Menu() {
   return(
     <main className='menu'>
       <h2 > Our Menu</h2>
-      <Pizza/>
-      <Pizza/>
-      <Pizza/>
-      <Pizza/>   
+      <Pizza name= 'Pizza Spinaci' ingredients= 'Tomato, mozarella, spinach, and ricotta cheese' photoName= "pizzas/spinaci.jpg" price={10}/>
+      <Pizza name= 'Pizza Funghi' ingredients= 'Tomato,  ricotta cheese' photoName= "pizzas/funghi.jpg" price={9} />
     </main>
   )
+}
+
+function Pizza (props){
+  console.log(props)
+  return <>
+  <div className='pizza'>
+    <img src={props.photoName} alt={props.name} />
+    <div>
+      <h3>{props.name}</h3>
+      <p>{props.ingredients}    </p>
+      <span>{props.price +3}</span>
+    </div>
+  </div>
+  </>
 }
 
 function Footer() {
