5ddbe25d43b8c2c9f472e26ae66449b5a64207d9
Yanina Amatore <yanina.amatore@datacom.co.nz>
2024-02-28 10:59:26 +1300

    Add map method

--- src/index.js
+++ src/index.js
@@ -77,8 +77,11 @@ function Menu() {
   return(
     <main className='menu'>
       <h2 > Our Menu</h2>
-      <Pizza name= 'Pizza Spinaci' ingredients= 'Tomato, mozarella, spinach, and ricotta cheese' photoName= "pizzas/spinaci.jpg" price={10}/>
-      <Pizza name= 'Pizza Funghi' ingredients= 'Tomato,  ricotta cheese' photoName= "pizzas/funghi.jpg" price={9} />
+      <ul className='pizzas'>
+        {pizzaData.map( pizza => <Pizza pizzaObj= {pizza} key={pizza.name} />          )}
+      </ul>
+      {/* <Pizza name= 'Pizza Spinaci' ingredients= 'Tomato, mozarella, spinach, and ricotta cheese' photoName= "pizzas/spinaci.jpg" price={10}/>
+      <Pizza name= 'Pizza Funghi' ingredients= 'Tomato,  ricotta cheese' photoName= "pizzas/funghi.jpg" price={9} /> */}
     </main>
   )
 }
@@ -86,14 +89,14 @@ function Menu() {
 function Pizza (props){
   console.log(props)
   return <>
-  <div className='pizza'>
-    <img src={props.photoName} alt={props.name} />
+  <li className='pizza'>
+    <img src={props.pizzaObj.photoName} alt={props.pizzaObj.name} />
     <div>
-      <h3>{props.name}</h3>
-      <p>{props.ingredients}    </p>
-      <span>{props.price +3}</span>
+      <h3>{props.pizzaObj.name}</h3>
+      <p>{props.pizzaObj.ingredients}    </p>
+      <span>{props.pizzaObj.price +3}</span>
     </div>
-  </div>
+  </li>
   </>
 }
 
